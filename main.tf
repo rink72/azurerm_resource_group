f27f8cad756b629cfaa8b177d47741a06c326474
Sam <beardmans72@gmail.com>
2019-12-23 13:51:44 +1300

    Add terraform required version

--- main.tf
+++ main.tf
@@ -2,6 +2,10 @@ provider "azurerm" {
   version = ">= 1.39.0"
 }
 
+terraform {
+  required_version = ">= 0.12.0"
+}
+
 resource "azurerm_resource_group" "rg" {
   name     = var.name
   location = var.location
